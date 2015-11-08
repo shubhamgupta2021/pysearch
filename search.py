e9372d58505013ac5141a6c48a515c69100ee4bb
shubhamgupta2021 <shubham.gupta2021@gmail.com>
2015-11-08 19:50:58 +0530

    modified datastructure to dictionary

--- search.py
+++ search.py
@@ -5,6 +5,9 @@ from getopt import getopt, GetoptError
 import sys
 from os import walk, path
 
+
+d = {}
+
 def get_attributes(argv):
     try:
         opts, args = getopt(argv, "hf:d:", ["file", "directory"])
@@ -47,17 +50,25 @@ def find_synonyms(word):
     synonyms = set(synonyms)
     return  list(synonyms)
 
-def search_in_file(file, words_to_search):
+def search_in_file(file):
     try:
         with open(file,'r') as fp:
             print file
             for line in fp:
-                tokens = word_tokenize(line)
-                for word in words_to_search:
-                    if word in tokens:
-                        print word
-                        print line
-                        break
+                for word in word_tokenize(line):
+                    if word in d:
+                        d[word].append(line)
+
+            print "Results found in %s file" %(file)
+            print
+            for word in d:
+                count=len(d[word])
+                if count > 0:
+                    print "%d results found for %s" %(count, word)
+                    for line in d[word]:
+                      print line
+                    d[word] = []
+
     except IOError as e:
         print "Error: Cannot Open File"
         sys.exit(e.errno)
@@ -67,17 +78,19 @@ def main():
     (source_type, source, words_to_search) = get_attributes(args)
     words_with_synonyms = []
     for word in words_to_search:
-        words_with_synonyms += find_synonyms(word)
+        for synonm in  find_synonyms(word):
+            d[synonm] = []
+        #words_with_synonyms += find_synonyms(word)
     if source_type == 'directory':
         all_files = []
         for (directorypath, directories, files) in walk(source):
             files = [ path.join(directorypath, file) for file in files ]
             all_files = all_files + files
         for file in all_files:
-            search_in_file(file, words_with_synonyms)
+            search_in_file(file)
 
     if source_type == 'file':
-        search_in_file(source, words_with_synonyms)
+        search_in_file(source)
 
 
 if __name__ == "__main__":
